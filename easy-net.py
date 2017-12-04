c49a8ccef1e45b8ef41f6edf6ee17f306de805b6
Benedikt Rauch <benediktrauch@users.noreply.github.com>
2017-12-04 09:33:11 +0100

    improved plot added learning animation

--- easy-net.py
+++ easy-net.py
@@ -2,13 +2,33 @@ import random
 import math
 import tools
 import matrix
-import time
 
+# Loading import
+import itertools
+import threading
+import sys
+
+# Plot import
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 
+# Learning animation
+# thanks to Andrew Clark on
+# https://stackoverflow.com/questions/22029562/python-how-to-make-simple-animated-loading-while-process-is-running
+def animate():
+    for c in itertools.cycle(['.', '..', '...', '   ']):  # ['|', '/', '-', '\\']
+        if done:
+            break
+        sys.stdout.write("\rI'm  learning " + c)
+        sys.stdout.flush()
+        time.sleep(0.5)
+    sys.stdout.write('\rDone!                               ')
+
+
+# My net
 def main(x, hidden, b, learning, test, w, e, n_d):
     random.seed(1)
     training_data = x[:]
@@ -26,12 +46,13 @@ def main(x, hidden, b, learning, test, w, e, n_d):
     synapses1 = []
 
     for _ in range(hidden):
-        synapses0.append([random.uniform(w, -w), random.uniform(w, -w)])
+        synapses0.append([random.uniform(w, -w), random.uniform(w, -w)])  # second rand for bias synapses
     for j in range(hidden + 1):  # +1 for bias
         synapses1.append([random.uniform(w, -w)])
 
     sig_layer2 = []
 
+    # learning loop (learning = iterations)
     for i in xrange(learning):
 
         # # # Forward pass
@@ -98,15 +119,11 @@ def main(x, hidden, b, learning, test, w, e, n_d):
     # Testing net with noised data
     sig_noise = []
     if len(n_d) > 0:
-        print "testing with noise data"
+        # print "testing with noise data"
 
         l1 = matrix.multiply(synapses0, noise_data)
 
-        # Activation level
         sig_l1 = matrix.sig(l1)
-
-        # # Hidden Layer
-        # Adding bias to layer1
 
         b_sig_l1 = sig_l1[:]
 
@@ -119,34 +136,35 @@ def main(x, hidden, b, learning, test, w, e, n_d):
 
         sig_noise = matrix.sig(l2)
 
-    # formatting output for plot
-    result1 = []
-    result2 = []
+    # formatting net output for plot
+    result1 = []  # training data
+    result2 = []  # noised data
     for i in range(len(sig_layer2[0])):
         result1.append(sig_layer2[0][i] * 2 - 1)
         result2.append(sig_noise[0][i] * 2 - 1)
 
     # Plot
     neuron_patch = mpatches.Patch(label='Neurons: '+str(hidden))
-    # bias_patch = mpatches.Patch(label='Bias: '+str(b))
+    bias_patch = mpatches.Patch(label='Bias: '+str(b))
     iteration_patch = mpatches.Patch(label='Iterations: '+str(learning))
     epsilon_patch = mpatches.Patch(label='Epsilon: '+str(e))
+    weight_patch = mpatches.Patch(label='Weight range (0 +/-): '+str(w))
     time_patch = mpatches.Patch(label=str(round((time.time() - start_time)/60, 2)) + " min")
+    first_legend = plt.legend(handles=[bias_patch, time_patch, epsilon_patch, neuron_patch, iteration_patch, weight_patch],
+                              bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+                              ncol=3, mode="expand", borderaxespad=0.)
 
-    plt.legend(handles=[neuron_patch, iteration_patch, epsilon_patch, time_patch], bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-    plt.plot(inputData[0], result1)
-    plt.plot(inputData[0], result2)
-    plt.plot(x_data, y_data)
+    line1, = plt.plot(inputData[0], result1, label="Training Data", linewidth=0.75)
+    line2, = plt.plot(inputData[0], result2, label="Test Data", linestyle=':', linewidth=0.75)
+    line3, = plt.plot(x_data, y_data, label="sin(x)", linestyle='--', linewidth=0.75)
+    ax = plt.gca().add_artist(first_legend)
+    plt.legend(handles=[line1, line2, line3])
     plt.savefig('./plots/plot'+str(time.time())+'.png')
 
     plt.clf()
     plt.cla()
     plt.close()
 
-    # plt.legend(handles=[neuron_patch, bias_patch, iteration_patch, epsilon_patch],
-    # bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-    # plt.plot(inputData[0], result)
-    # plt.show()
 
 # inputData = [[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]]
 
@@ -162,17 +180,27 @@ noise_d = [tools.linspace(0.1, 6.5, 50)]
 x_data = inputData[0]
 y_data = np.sin(x_data)
 
-iterations = 100000
+iterations = 5000
 hiddenNeurons = 13
 bias = 1.
 weight = 0.95
-epsilon = 0.31
+epsilon = 0.41
 
-for _ in range(15):
-    epsilon += 0.01
+for _ in range(1):
+    done = False
 
-    print "epsilon : " + str(epsilon)
-
+    t = threading.Thread(target=animate)
+    t.start()
+    # epsilon += 0.01
+    # print "epsilon : " + str(epsilon)
     start_time = time.time()
 
     main(inputData, hiddenNeurons, bias, iterations, testdata, weight, epsilon, noise_d)
+
+    time.sleep(10)
+    done = True
+
+
+
+
+
